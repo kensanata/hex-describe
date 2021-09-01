62bacb8baed93fd96249e6525510ce557c46e6b4
Alex Schroeder <alex@gnu.org>
2021-09-01 22:20:53 +0200

    First commit
    Based on the hex-mapping repo.
    https://alexschroeder.ch/cgit/hex-mapping/

---
+++ lib/Game/HexDescribe.pm
@@ -0,0 +1,23 @@
+package Game::HexDescribe;
+
+our $VERSION = 1.00;
+
+=head1 NAME
+
+Game::HexDescribe - a web app to add random table driven data to map data
+
+=head1 DESCRIPTION
+
+This is a stub module, see F<script/hex-describe> for details of the app.
+
+=head1 AUTHOR
+
+Alex Schroeder
+
+=head1 LICENSE
+
+GNU Affero General Public License
+
+=cut
+
+1;
